5a3a7014679694468b0b2b98a901710721dc0e20
Ubuntu <ubuntu@ip-172-31-23-26.ec2.internal>
2025-03-07 12:11:10 +0000

    settings change

--- Suggested_Products/Suggested_Products/settings.py
+++ Suggested_Products/Suggested_Products/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-9pkp*h$aguczr^!c=8d275odd9+^w0aab!5uq^+90_)02jqhk3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
